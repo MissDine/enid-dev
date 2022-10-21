a87f6a2e7b5eebfeb4c5f383dd02c07b4c418b63
Enid Nyatichi <enidnyatichi1212@gmail.com>
2022-10-21 17:34:01 +0300

    Feature edit

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,28 +2,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from "next/image";
+import github from "../images/icons8-github-64.png"
+import linkedin from "../images/icons8-linkedin-64.png"
+import email from "../images/icons8-email-64.png"
+import call from "../images/icons8-call-50.png"
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" >
       <Container>
         <Navbar.Brand href="#home">Enid</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#github"><Image src={github} alt="" /></Nav.Link>
+            <Nav.Link href="#github"><Image src={linkedin} alt="" /></Nav.Link>
+            <Nav.Link href="#github"><Image src={email} alt="" /></Nav.Link>
+            <Nav.Link href="#github"><Image src={call} alt="" /></Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#home">Home</Nav.Link>
