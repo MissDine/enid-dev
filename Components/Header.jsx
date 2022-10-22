e0e8d239b862f5c6d0b6e6f648feb4967bf703c0
Enid Nyatichi <enidnyatichi1212@gmail.com>
2022-10-22 12:56:38 +0300

    Feature edit

---
+++ Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Typed from "react-typed";
+import Carousel from 'react-bootstrap/Carousel';
+import Image from "next/image"
+import eni from "../images/Missdine.jpeg"
+
+function Header() {
+  return (
+    <div className="col-sm-12" style={{backgroundColor:"#66BFBF"}}>
+      <div className="row" style={{justifyContent:"center",alignItems:"center"}}>
+        <div className="col-md-6" style={{height:"400px", width:"300px", marginTop:"6vh"}}>
+          <Image src={eni} alt="" style={{height:"10px", width:"10px",borderRadius:"50%"}}/>
+        </div>
+        <div className="col-md-6" style={{marginTop:"-2vh", marginLeft:"3vw"}}>
+          <h1>My name is Enid Nyatichi,</h1>
+          <h1>And I am a <span>Software Developer</span></h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from "../Components/Navbar.jsx"
+import  Header from "../Components/Header.jsx"
+
 
 export default function Home() {
   return (
@@ -12,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar/>
+      <Header/>
     </div>
   )
 }
