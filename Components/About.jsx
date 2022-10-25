66ab6b511fc21bd30797648760a37e3f5fec33b7
Enid Nyatichi <enidnyatichi1212@gmail.com>
2022-10-25 14:16:02 +0300

    Feature edit

--- Components/About.jsx
+++ Components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='col-sm-12' style={{backgroundColor:"#EAF6F6",height:"300px"}} id="about">
-        <h1>About Me</h1>
-        <p>Enid is an enthusiastic, self-motivated and skilled full-stack developer who enjoys designing and developingsolutions that satisfy the needs of the customer. She also enjoys learning new technologies, tools and methodologies that enable her to develop more efficiently.</p>
+    <div className='col-sm-12' style={{backgroundColor:"#EAF6F6",height:"300px",textAlign:"center"}} id="about">
+        <h1 style={{fontSize:"1rem",fontWeight:"600",color:"#FF0063"}}>About Me</h1>
+        <p style={{fontSize:"1rem",marginLeft:"15vw",marginRight:"15vw"}}>Enid is an enthusiastic, self-motivated and skilled full-stack developer who enjoys designing and developingsolutions that satisfy the needs of the customer. She also enjoys learning new technologies, tools and methodologies that enable her to develop more efficiently.</p>
         <button style={{marginLeft:"2vw"}}><a href="#https://enid-cv.vercel.app/">View CV</a></button>
     </div>
   )
--- Components/Header.jsx
+++ Components/Header.jsx
@@ -8,23 +8,24 @@ function Header() {
   return (
     <div className="col-sm-12" style={{backgroundColor:"#66BFBF"}} id="header">
       <div className="row" style={{justifyContent:"center",alignItems:"center"}}>
-        <div className="col-md-6" style={{height:"400px", width:"300px", marginTop:"6vh"}}>
+        <div className="col-md-6" style={{height:"400px", width:"300px", marginTop:"11vh"}}>
           <Image src={eni} alt="" style={{height:"10px", width:"10px",borderRadius:"50%"}}/>
         </div>
         <div className="col-md-6" style={{marginTop:"-2vh", marginLeft:"3vw"}}>
-          <h1>My name is Enid Nyatichi,</h1>
-          <h1>And I am a  
+          <h1 style={{fontSize:"2rem"}}>My name is Enid Nyatichi,</h1>
+          <h1 style={{fontSize:"2rem"}}>And I am a  
           <Typed className="typ"
           strings={[
             " Fullstack Software Developer"
             // "We value our students"
           ]}
           typeSpeed={150}
-          backSpeed={150}
+          backSpeed={250}
           loop
+          style={{color:"#FF0063"}}
         />
           </h1>
-          <p>Hire Me!</p>
+          <button style={{backgroundColor:"#FF0063",border:"solid 0px",height:"5vh",width:"7vw",color:"#FFFFFF",borderRadius:"30px"}}><p>Hire Me!</p></button>
         </div>
       </div>
     </div>
